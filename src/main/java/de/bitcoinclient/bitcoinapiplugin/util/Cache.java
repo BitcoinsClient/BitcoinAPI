ffb52d76dbe05a122e2afbc0e0987faf0b3e1073
BitcoinsClient <98539603+BitcoinsClient@users.noreply.github.com>
2022-12-17 16:43:58 +0100

    Delete src directory

--- src/main/java/de/bitcoinclient/bitcoinapiplugin/util/Cache.java
+++
@@ -1,9 +0,0 @@
-package de.bitcoinclient.bitcoinapiplugin.util;
-
-import com.mongodb.client.MongoDatabase;
-
-import java.util.HashMap;
-
-public class Cache {
-    public static HashMap<String, MongoDatabase> databaseHashMap = new HashMap<>();
-}

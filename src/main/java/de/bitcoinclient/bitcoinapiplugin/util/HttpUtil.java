b4526d307b17de7a59fdb7362dc023ee9f51c5a3
BitcoinsClient <98539603+BitcoinsClient@users.noreply.github.com>
2022-12-17 13:55:52 +0100

    Add files via upload

--- src/main/java/de/bitcoinclient/bitcoinapiplugin/util/HttpUtil.java
+++ src/main/java/de/bitcoinclient/bitcoinapiplugin/util/HttpUtil.java
@@ -8,27 +8,6 @@ import java.net.URL;
 
 public class HttpUtil {
 
-    public static void requestAPI() {
-
-    }
-
-    public static void login(String password, String channel) {
-        try {
-            URL url = new URL("updateserver.bitcoinclient.de:8080/users/user=api" + "/password=" + password + "/channel=" + channel);
-            url.openConnection();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String requestAPIVersion(){
-        return getHttp("updateserver.bitcoinclient.de:8080/version").split(",")[0].split("#")[1];
-    }
-
-    public static String requestPluginVersion(){
-        return getHttp("updateserver.bitcoinclient.de:8080/version").split(",")[1].split("#")[1];
-    }
-
     private static String getHttp(String url) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
